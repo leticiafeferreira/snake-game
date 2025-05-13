a4292e79ab4a2f3e6fb131020e092ef4f7a6ceaf
lehh <leticia.fer.veleiros@gmail.com>
2025-05-13 16:07:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -52,7 +52,74 @@ const updateFoodPosition = () => {
 ou com as paredes do tabuleiro */
 
 const handleGameOver = () => {
-    clearInterval(setIntervalId);
+    clearInterval(setIntervlId);
     alert("Game Over! 😫 Aperto Ok para iniciar novamente... ");
     location.reload();
 }
+
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+//Começar o game 
+const initGame = () => {
+    if (gameOver) return handleGameOver ();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    //Quando a cobra se alimentaa
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]);
+        score++;
+        higtScore = score >= higtScore ? score : higtScore
+
+        localStorage.setItem("high-score" , higtScore);
+        scoreElement.innerHTML = `Score: ${score}`;
+        highScoreElement.innerHTML = `High Score: ${higtScore}`;
+
+    }
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    for (let i = snakeBody.length - 1; i > 0; i --) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
+
+
+    //add div para cada parte do corpo da cobra 
+    for (let i = 0; i <snakeBody.length; i++) {
+        html += `<div class="head" style="grid-area: ${snakeBody [i][1]} / ${snakeBody [i][0]}"></div>`;
+
+        //verificar se a cabeça da cobra atingiu ou colidiu com o corpo 
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true
+        }
+        paybord.innerHTML = html;
+
+    }
+
+}
+
+updateFoodPosition();
+setIntervlId = setInterval (initGame, 100);
+document.addEventListener("keyup", changeDirection);
